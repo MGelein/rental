9114945550d90d30e9a6ea5d7bc7116286fb6e10
Mees Gelein <meesgelein@gmail.com>
2018-05-18 00:03:59 +0200

    Continnued work on the layout... quite alot of work

--- js/script.js
+++ js/script.js
@@ -5,4 +5,7 @@
 $(document).ready(function(){
     //Prevent caching of Ajax requests
     $.ajaxSetup({cache: false});
+
+    //Now initialize all tooltips in the file
+    $('[data-toggle="tooltip"]').tooltip();
 });
