875ced70efd827f3556c2c0b057f8bab0d546074
Mees Gelein <meesgelein@gmail.com>
2018-05-18 17:35:56 +0200

    Working on more layout and making hte updates happen

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ var rentalItem;
 var rentals;
 /**Contains the latest timestamp of the rentals update from the server */
 var timestamp = 0;
+/**Value that decides if we see rentals that have been closed*/
+var showClosed = false;
 
 /**
  * The ready function for the whole doucment. This is run once the whole
@@ -29,7 +31,7 @@ $(document).ready(function(){
     });
 
     //Start the loop to update any changes from the rentals
-    setInterval(updateRentals, 2000);
+    setInterval(updateRentals, 1000);
 });
 
 /**
@@ -44,13 +46,39 @@ function updateRentals(){
         //Then compare to current timestamp, if the current timestamp is older, please update
         if(ts > timestamp){
             //Retrieve the list of rentals from the backend
-            listRentals("all", function(data){
+            listRentals((showClosed) ? "all" : "open", function(data){
                 //Only update the timestamp once we have the data
                 timestamp = ts;
                 //Then set the data to the global variable
                 rentals = data;
-                console.log("Rentals succesfully updated");
+                console.log("Rentals succesfully received, updating screen");
+                updateRentalOverview();
             });
         }
     });
+}
+
+/**
+ * Handles the updating of the rental overview. This constructs a list of 
+ * rentals received from the server using the rentalList template.
+ */
+function updateRentalOverview(){
+    //The start of the HTML we will be injecting
+    var html = "";
+    //Now go through every rental and render it to the overview
+    $.each(rentals, function(index, rental){
+        var line = rentalItem.replace(/%IDNUMBER%/g, rental.id);
+        line = line.replace(/%STARTDATE%/g, rental.startDate);
+        line = line.replace(/%ENDDATE%/g, rental.endDate);
+        line = line.replace(/%ITEMS%/g, rental.items.join(', '));
+        line = line.replace(/%WARNING%/g, rental.warning);
+        //Append this line to the html
+        html += line;
+    });
+
+    //Now set the HTML of the rentalOverview
+    $('#rentalList').html(html);
+
+    //Log the event
+    console.log("Rental overview updated.");
 }
